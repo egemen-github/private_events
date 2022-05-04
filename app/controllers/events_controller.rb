acae34ac4e6f2bca1146df48027b5bb0d723ccaf
Egemen <egemenwasd@gmail.com>
2022-05-04 14:11:49 +0300

    event create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,23 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def new
+        @event = current_user.events.new
+    end
+
+    def create
+        @event = current_user.events.new(event_params)
+        if @event.save
+          redirect_to @event
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
+
+      private
+    
+      def event_params
+        params.require(:event).permit(:title, :description, :location, :date, :time)
+      end
 end
