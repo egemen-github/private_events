69d16ab58db5a3564cc418ec30c6d62b1c95e58a
Egemen <egemenwasd@gmail.com>
2022-05-05 00:02:26 +0300

    validation and association update but not finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :events, foreign_key: 'creator', class_name: 'Event'
+  has_many :events, foreign_key: 'creator', class_name: 'Event', dependent: :destroy
 end
